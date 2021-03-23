6a4322b9619f79b24bb16f43275994193a2d1979
bobo <bobo.diallo@m2t.biz>
2021-03-23 10:56:12 +0000

    feat: adding README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+## Gestion agence immobilière
---
+++ src/Entity/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface, \Serializable
+{
+   /**
+    * @ORM\Id
+    * @ORM\GeneratedValue
+    * @ORM\Column(type="integer")
+    */
+   private ?int $id;
+
+   /**
+    * @ORM\Column(type="string", length=255)
+    */
+   private ?string $username;
+
+   /**
+    * @ORM\Column(type="string", length=255)
+    */
+   private ?string $password;
+
+   public function getId(): ?int
+   {
+      return $this->id;
+   }
+
+   public function getUsername(): ?string
+   {
+      return $this->username;
+   }
+
+   public function setUsername(string $username): self
+   {
+      $this->username = $username;
+
+      return $this;
+   }
+
+   public function getPassword(): ?string
+   {
+      return $this->password;
+   }
+
+   public function setPassword(string $password): self
+   {
+      $this->password = $password;
+
+      return $this;
+   }
+
+   /**
+    * @return string[]
+    */
+   public function getRoles(): array
+   {
+      return ['ROLE_ADMIN'];
+   }
+
+   public function getSalt()
+   {
+      return null;
+   }
+
+   public function eraseCredentials()
+   {
+   }
+
+   /**
+    * @return string|null
+    */
+   public function serialize(): string
+   {
+      return serialize([
+         $this->id,
+         $this->username,
+         $this->password,
+      ]);
+   }
+
+   /**
+    * @param string $serialized
+    */
+   public function unserialize($serialized)
+   {
+      list(
+         $this->id,
+         $this->username,
+         $this->password
+         ) = unserialize($serialized, ['allowed_classes' => false]);
+   }
+}
